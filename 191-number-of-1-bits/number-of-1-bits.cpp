0b09c8f757b472885144b93b415cc66cf5c34fe4
S-Priyadharshan <145854138+S-Priyadharshan@users.noreply.github.com>
2025-02-11 00:07:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (47.49%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        return __builtin_popcount(n);
+        // return __builtin_popcount(n);
+        // Alternate approach using Brian Kernighan's Algorithm
+
+        int count=0;
+        while(n){
+            n=n&(n-1);
+            count++;
+        }
+        return count;
     }
 };
