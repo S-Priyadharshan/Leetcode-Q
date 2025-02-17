0f77844e2ccbb2e0960699f0ccfaa57eb4b7aa3e
S-Priyadharshan <145854138+S-Priyadharshan@users.noreply.github.com>
2025-02-17 19:50:07 +0530

    Time: 3 ms (40.56%) | Memory: 14.2 MB (66.96%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int>mp;
-        unordered_set<int>s;
-        vector<int>ans;
-        for(int i:nums1)mp[i]++;
-        for(int j:nums2){
-            if(mp.find(j)!=mp.end() && mp[j]>0){
-                mp[j]--;
-                s.insert(j);
-            }
-        }
-        for(int i:s)ans.push_back(i);
+        // unordered_map<int,int>mp;
+        // unordered_set<int>s;
+        // vector<int>ans;
+        // for(int i:nums1)mp[i]++;
+        // for(int j:nums2){
+        //     if(mp.find(j)!=mp.end() && mp[j]>0){
+        //         mp[j]--;
+        //         s.insert(j);
+        //     }
+        // }
+        // for(int i:s)ans.push_back(i);
+        // return ans;
+
+        // Alterate
+        unordered_set<int>s(nums1.begin(),nums1.end()); // No extra for loop
+        vector<int> ans;
+        for(int i:nums2)if(s.erase(i))ans.push_back(i); //s.erase(x) returns 1 if the value existed and erased
         return ans;
     }
 };
