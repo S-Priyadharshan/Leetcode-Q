a3133c733a9cd532e0ce478a2324b9ea1e3c6261
Priyadharshan <s.priyadharshan.official@gmail.com>
2025-02-17 18:56:25 +0530

    Revision #2

--- Arrays/169_Majority_Element/solution.cpp
+++ Arrays/169_Majority_Element/solution.cpp
@@ -4,5 +4,9 @@
 using namespace std;
 
 int main() {
+    // Apart from counting frequencies, More efficient method
+    vector<int> nums = {2,2,1,1,1,2,2};
+    sort(nums.begin(),nums.end());
+    cout << nums[nums.size()/2] << "\n";
     return 0;
 }
