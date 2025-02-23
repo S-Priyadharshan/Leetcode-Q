c285c958b03235eff7c565d885afd6a4a76f008a
S-Priyadharshan <145854138+S-Priyadharshan@users.noreply.github.com>
2025-02-23 12:53:18 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (58.19%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -4,21 +4,35 @@ public:
         if(s.size()==1)return 1;
         if(s.size()==0)return 0;
 
-        int maxc=1;
-        int count=1;
-        string curr = "";
-        curr=s[0];
-        for(int i=1;i<s.size();i++){
-            if(curr.find(s[i])==string::npos){
-                curr+=s[i];
-                count++;
+        // int maxc=1;
+        // int count=1;
+        // string curr = "";
+        // curr=s[0];
+        // for(int i=1;i<s.size();i++){
+        //     if(curr.find(s[i])==string::npos){
+        //         curr+=s[i];
+        //         count++;
+        //     }else{
+        //         curr=curr.substr(curr.find(s[i])+1);
+        //         curr+=s[i];
+        //         count=curr.size();
+        //     }
+        //     maxc=max(maxc,count);
+        // }
+        // return maxc;
+
+        int maxs=0;
+        unordered_map<char,int>mp;
+        int left=0;
+        for(int i=0;i<s.size();i++){
+            if(mp.find(s[i])!=mp.end()&&mp[s[i]]>=left){
+                left=mp[s[i]]+1;
+                mp[s[i]]=i;
             }else{
-                curr=curr.substr(curr.find(s[i])+1);
-                curr+=s[i];
-                count=curr.size();
+                mp[s[i]]=i;
+                maxs=max(maxs,(i-left+1));
             }
-            maxc=max(maxc,count);
         }
-        return maxc;
+        return maxs;
     }
 };
