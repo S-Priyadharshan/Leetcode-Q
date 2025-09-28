bd44d9ae8cd2e623d2ef833dd5453496e978ec2c
S-Priyadharshan <145854138+S-Priyadharshan@users.noreply.github.com>
2025-09-28 10:28:01 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (97.71%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -4,11 +4,8 @@ public:
         vector<int>a(26,0);
         for(char ch:magazine)a[ch-'a']++;
         for(char ch:ransomNote){
-            if(a[ch-'a']==0){
-                return false;
-            }else{
-                a[ch-'a']--;
-            }
+            if(a[ch-'a']==0)return false;
+            else a[ch-'a']--;
         }
         return true;
     }
