d3f216ee7a95937bb22589d8f5ecb7c8d41048ad
S-Priyadharshan <145854138+S-Priyadharshan@users.noreply.github.com>
2025-09-28 10:27:17 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (74.93%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int>a(26,0);
+        for(char ch:magazine)a[ch-'a']++;
+        for(char ch:ransomNote){
+            if(a[ch-'a']==0){
+                return false;
+            }else{
+                a[ch-'a']--;
+            }
+        }
+        return true;
+    }
+};
