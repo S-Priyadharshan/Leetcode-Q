b168b667251a34cb09df7308e59ac73f2a4bf1ba
S-Priyadharshan <145854138+S-Priyadharshan@users.noreply.github.com>
2025-09-30 19:10:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (3.05%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        for(char ch:letters){
-            if(ch>target)return ch;
-        }
-        return letters[0];
+        // for(char ch:letters){
+        //     if(ch>target)return ch;
+        // }
+        // return letters[0];
+        auto it=upper_bound(letters.begin(),letters.end(),target);
+        if(it!=letters.end())return *it;
+        else return letters[0];
     }
 };
