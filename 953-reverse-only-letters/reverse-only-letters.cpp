a6f2c77c3c9ae2f93824143fa5cc3eaa2775d911
S-Priyadharshan <145854138+S-Priyadharshan@users.noreply.github.com>
2025-10-01 18:51:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (83.64%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.cpp
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -1,12 +1,32 @@
+// class Solution {
+// public:
+//     string reverseOnlyLetters(string s) {
+//         int left = 0, right = s.size() - 1;
+//         while(left < right) {
+//             if(!isalpha(s[left])) left++;
+//             else if(!isalpha(s[right])) right--;
+//             else {
+//                 swap(s[left], s[right]);
+//                 left++;
+//                 right--;
+//             }
+//         }
+//         return s;
+//     }
+// };
+
 class Solution {
 public:
     string reverseOnlyLetters(string s) {
-        int left = 0, right = s.size() - 1;
-        while(left < right) {
-            if(!isalpha(s[left])) left++;
-            else if(!isalpha(s[right])) right--;
-            else {
-                swap(s[left], s[right]);
+        int n=s.size();
+        int left=0,right=n-1;
+        while(left<right){
+            if(!((s[left]>='a'&&s[left]<='z')||(s[left]>='A'&&s[left]<='Z'))){
+                left++;
+            }else if(!((s[right]>='a'&&s[right]<='z')||(s[right]>='A'&&s[right]<='Z'))){
+                right--;
+            }else{
+                swap(s[left],s[right]);
                 left++;
                 right--;
             }
