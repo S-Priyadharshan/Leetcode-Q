7044dd7a1c7f543c2d1588a45f62e22ce6664e96
S-Priyadharshan <145854138+S-Priyadharshan@users.noreply.github.com>
2026-02-03 11:25:31 +0530

    Time: 62 ms (3.34%) | Memory: 20.6 MB (67.71%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+     int l=0,r=0;
+     int n=nums.size();
+     while(l<n && r<n){
+        while(l<n && nums[l]%2==0)l++;
+        while(r<n && (r<=l || nums[r]%2!=0))r++;
+        cout<<l<<" "<<r<<"\n";
+        if(l<n && r<n)swap(nums[l],nums[r]);
+        r++;
+     }
+     return nums;   
+    }
+};
